68b613664c567a96cb88d701c7b10817eed469fa
Linus <leetamparega@gmail.com>
2023-03-15 11:58:49 +0000

    My first Hello World program

---
+++ helloworld.cpp
@@ -0,0 +1,16 @@
+
+//This is for C++
+#inlcude<iostream>
+
+using namespace std;
+
+int main{
+    cout<< "Hello World"<<endl;
+    return 0;
+}
+
+//This is for python
+print("Hello World")
+
+//This is for ruby
+puts "Hello World"
